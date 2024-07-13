0438c551b1609ca95b0076dbe9d0eef07c4810ee
techgirl027 <noilaxonnematjonova@gmail.com>
2024-07-13 17:03:11 +0500

    second commit

--- main/admin.py
+++ main/admin.py
@@ -2,10 +2,5 @@ from django.contrib import admin
 from .models import Message, TopBooks
 
 
-# class MessageAdmin(admin.ModelAdmin):
-#     list_display = ["name", "email", "created_at"]
-#     search_fields = ["name", "email"]
-#     list_filter = ["created_at"]
-
 admin.site.register(Message)
 admin.site.register(TopBooks)
